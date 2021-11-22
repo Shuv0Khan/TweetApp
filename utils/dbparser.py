2d78614342344d079da06a1d97bac41c088eac4a
Shuv0Khan <naweemshuvo@gmail.com>
2021-11-22 14:37:01 -0500

    public metrics with/without user names

--- utils/dbparser.py
+++ utils/dbparser.py
@@ -18,6 +18,7 @@ def get_all_tweets():
             for tweet_list in data_list["data"]:
                 logging.debug(tweet_list["text"])
 
+
 def get_all_unique_author_ids():
     mongodb = mongodb_processor.MongoDBProcessor(constants.mongodb_url, constants.mongodb_db_name).my_db
     collections = mongodb.list_collection_names()
@@ -36,12 +37,33 @@ def get_all_unique_author_ids():
         logging.debug(key)
 
 
+def get_all_user_follower_following_counts():
+    mongodb = mongodb_processor.MongoDBProcessor(constants.mongodb_url, constants.mongodb_db_name).my_db
+    collection = mongodb.get_collection(name='users_info')
+    cursor = mongodb['users_info'].find({}, {'data.id': 1, 'data.name': 1, 'data.public_metrics': 1, '_id': 0})
+    out_wname = open("users_with_name.csv", "w")
+    out_wt = open("users.csv", "w")
+
+    for data_list in cursor:
+        for key in data_list:
+            for data in data_list[key]:
+                out_wname.write(
+                    f"{data['id']},{data['name']},{data['public_metrics']['followers_count']},{data['public_metrics']['following_count']},{data['public_metrics']['tweet_count']},{data['public_metrics']['listed_count']}\n")
+                out_wt.write(
+                    f"{data['id']},{data['public_metrics']['followers_count']},{data['public_metrics']['following_count']},{data['public_metrics']['tweet_count']}\n")
+
+    out_wname.close()
+    out_wt.close()
+
+
 def main():
     logging.basicConfig(filename='DBParser.log',
                         format='%(message)s',
                         level=logging.DEBUG)
 
-    get_all_unique_author_ids()
+    # get_all_unique_author_ids()
+    get_all_user_follower_following_counts()
+
 
 if __name__ == '__main__':
     main()
