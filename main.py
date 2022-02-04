422f909760eb130da271cf3339f0073ca9518722
Shuv0Khan <naweemshuvo@gmail.com>
2022-02-03 21:09:27 -0500

    streamlining with run arguments

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import logging
 import time
 import traceback
 
+from utils import dbparser
+from twitterdev import search_tweet_interface
 from twitterdev import api_processor
 from utils import constants
 import smtplib, ssl
 
 
-def main():
+def main(choice):
+    if choice is None:
+        print("Please provide the appropriate choice of operation.")
+        return
+
     logging.basicConfig(filename='TwitterApp.log',
                         format='%(asctime)s::%(levelname)s: %(message)s',
                         datefmt='%m/%d/%Y %I:%M:%S %p',
@@ -23,23 +29,47 @@ def main():
     logging.debug("negative_keywords = {}".format(constants.negative_keywords))
     logging.debug(f"negative key - {len(constants.negative_keywords)}")
 
-    # raise Exception("Testing email")
+    logging.debug(f"\n***************************\nSelected Operation: {choice}\n***************************\n")
 
-    apiProcessor = api_processor.ApiProcessor()
-    # apiProcessor.get_followers()
-    apiProcessor.get_followings()
-    # apiProcessor.start_twitterdev()
+    if choice == 'test':
+        logging.debug("Choice captured successfully")
 
-    # full_archive_search = search_tweet_interface.FullArchiveSearch()
-    # full_archive_search.do_get()
-    # full_archive_search.do_count()
+    elif choice == 'test_email':
+        raise Exception("Testing email")
 
-    # dbparser.get_all_tweets()
+    elif choice.startswith('api'):
+        api = api_processor.ApiProcessor()
+
+        if choice == 'api_get_followers':
+            api.get_followers()
+        elif choice == 'api_get_followings':
+            api.get_followings()
+        elif choice == 'api_get_through_relay':
+            api.get_followings_through_relay()
+        elif choice == 'api_twitterdev':
+            api.start_twitterdev()
+
+    elif choice.startswith('full'):
+        full_archive_search = search_tweet_interface.FullArchiveSearch()
+        if choice == 'full_get':
+            full_archive_search.do_get()
+        elif choice == 'full_count':
+            full_archive_search.do_count()
+
+    elif choice.startswith('db'):
+        if choice == 'db_all_tweets':
+            dbparser.get_all_tweets()
 
 
 if __name__ == '__main__':
+    from sys import argv
+
+    choice = None
+    if len(argv) == 2:
+        choice = argv[1]
+
     try:
-        main()
+        main(choice)
     except Exception as e:
         traceback.print_exc()
         logging.error(e)
