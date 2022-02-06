fd6c0fe3d60def8b7399399ee4fb4e396b5331cf
Shuv0Khan <naweemshuvo@gmail.com>
2022-02-06 15:34:26 -0500

    all user metrics collection

--- utils/dbparser.py
+++ utils/dbparser.py
@@ -37,23 +37,38 @@ def get_all_unique_author_ids():
         logging.debug(key)
 
 
-def get_all_user_follower_following_counts():
+def get_all_user_metrics():
     mongodb = mongodb_processor.MongoDBProcessor(constants.mongodb_url, constants.mongodb_db_name).my_db
     collection = mongodb.get_collection(name='users_info')
-    cursor = mongodb['users_info'].find({}, {'data.id': 1, 'data.username': 1, 'data.public_metrics': 1, '_id': 0})
-    out_wname = open("users_with_name.csv", "w")
-    out_wt = open("users.csv", "w")
+    cursor = mongodb['users_info'].find({}, {
+        'data.id': 1, 'data.username': 1, 'location': 1, 'data.public_metrics': 1,
+        'protected': 1, 'created_at': 1, 'profile_image_url': 1, 'verified': 1, '_id': 0
+    })
+    with open("users_metrics.csv", mode="w", encoding="utf8") as out:
+        out.write(
+            'id, username, location, followers, followings, tweets, listed, protected, created_at, verified, pp_url\n')
+        for data_list in cursor:
+            for key in data_list:
+                for data in data_list[key]:
+                    loc = '_NA_'
+                    if 'location' in data:
+                        loc = data['location']
 
-    for data_list in cursor:
-        for key in data_list:
-            for data in data_list[key]:
-                out_wname.write(
-                    f"{data['id']},{data['username']},{data['public_metrics']['followers_count']},{data['public_metrics']['following_count']},{data['public_metrics']['tweet_count']},{data['public_metrics']['listed_count']}\n")
-                out_wt.write(
-                    f"{data['id']},{data['public_metrics']['followers_count']},{data['public_metrics']['following_count']},{data['public_metrics']['tweet_count']}\n")
+                    line = str(data['id']) + ', ' + \
+                           data['username'] + ', ' + \
+                           loc + ', ' + \
+                           str(data['public_metrics']['followers_count']) + ', ' + \
+                           str(data['public_metrics']['following_count']) + ', ' + \
+                           str(data['public_metrics']['tweet_count']) + ', ' + \
+                           str(data['public_metrics']['listed_count']) + ', ' + \
+                           str(data['protected']) + ', ' + \
+                           str(data['created_at']) + ', ' + \
+                           str(data['verified']) + ', ' + \
+                           str(data['profile_image_url']) + '\n'
 
-    out_wname.close()
-    out_wt.close()
+                    out.write(line)
+                    return
+
 
 def get_all_users_bio():
     mongodb = mongodb_processor.MongoDBProcessor(constants.mongodb_url, constants.mongodb_db_name).my_db
@@ -65,14 +80,16 @@ def get_all_users_bio():
         for key in data_list:
             for data in data_list[key]:
                 bio = data['description'].strip()
-                bio = bio.replace('\n',' ')
+                bio = bio.replace('\n', ' ')
                 out.write(f"{data['id']}\t{bio}\n")
 
     out.close()
 
+
 def neo_user_info_transfer():
-    userset={}
-    with open("users_with_username.csv", mode="r", encoding="utf8") as unf, open("userset.csv", mode="r", encoding="utf8") as fset:
+    userset = {}
+    with open("users_with_username.csv", mode="r", encoding="utf8") as unf, open("userset.csv", mode="r",
+                                                                                 encoding="utf8") as fset:
         lines = unf.readlines()
         for line in lines:
             parts = line.strip().split(",")
@@ -101,11 +118,13 @@ def neo_user_info_transfer():
         count = 0
         for id in userset.keys():
             if userset[id]['take'] == 1:
-                cypher = 'MERGE (a:User {id: "'+id+'"}) ON CREATE SET a.name="'+userset[id]['username']+'", a.followers='+userset[id]['followers']+', a.following='+userset[id]['following']+', a.tweets='+userset[id]['tweets']
+                cypher = 'MERGE (a:User {id: "' + id + '"}) ON CREATE SET a.name="' + userset[id][
+                    'username'] + '", a.followers=' + userset[id]['followers'] + ', a.following=' + userset[id][
+                             'following'] + ', a.tweets=' + userset[id]['tweets']
                 # print(f"cypehr = {cypher}")
                 # print(f"response = {neo.query(cypher)}")
                 neo.query(cypher)
-                count+=1
+                count += 1
                 print(f"--- {count}")
 
 
@@ -115,9 +134,9 @@ def main():
                         level=logging.DEBUG)
 
     # get_all_unique_author_ids()
-    # get_all_user_follower_following_counts()
+    get_all_user_metrics()
     # neo_user_info_transfer()
-    get_all_users_bio()
+    # get_all_users_bio()
 
 
 if __name__ == '__main__':
